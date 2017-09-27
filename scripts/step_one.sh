c75aff324af5b364b76c385764fb73c7917e89b3
Peter Bohning <peter.bohning@gmx.com>
2017-09-27 18:13:17 -0400

    more graceful

--- scripts/step_one.sh
+++ scripts/step_one.sh
@@ -5,19 +5,39 @@ if [ $UID == 0 ] ; then
 	exit
 fi
 
-git clone https://github.com/MiCode/Xiaomi_Kernel_OpenSource
-echo "Downloading and extracting linux kernel..."
-curl http://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.2.tar.xz > linux-4.13.2.tar.xz
-tar xJvf linux-4.13.2.tar.xz 
-ln -sf Xiaomi_Kernel_OpenSource kernel
+die()
+{
+	echo $1
+	exit
+}
+
+#git clone https://github.com/MiCode/Xiaomi_Kernel_OpenSource
+#ln -sf Xiaomi_Kernel_OpenSource kernel
+echo "Linux kernel..."
+if ! [ -e linux-4.13.2 ]; then
+	if ! [ -e linux-4.13.2.tar.gz]; then
+		echo "Downloading and extracting linux kernel..."
+		if ! [ `curl http://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.2.tar.xz > linux-4.13.2.tar.xz`]; then
+			die "Can't get linux source"
+		fi
+	fi
+	tar xJvf linux-4.13.2.tar.xz 
+fi
+
 
 mkdir -p toolchain
 
-echo "Downloading and extracting linaro toolchain..."
+echo "Linaro toolchain..."
 LINARO_SITE=https://releases.linaro.org/components/toolchain/binaries/latest/aarch64-linux-gnu/
 LINARO=gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu
-if curl -L -O $LINARO_SITE$LINARO.tar.xz ; then
-cd toolchain; tar xJvf ../$LINARO.tar.xz 
+if ! [ -e toolchain/$LINARO ]; then
+	echo "Downloading and extracting linaro toolchain..."
+	if ! [ `curl -L -O $LINARO_SITE$LINARO.tar.xz` ] ; then
+		die "Can't get linaro toolchain"
+	fi
+	cd toolchain; tar xJvf ../$LINARO.tar.xz 
 fi
 
 echo "Compile kernel"
+echo "Ideally there should be a standard linux kernel and a .config for the particular phone"
+echo "hardware.  Also any dts/dtsi files should be copied in and compiled in"
